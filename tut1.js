eda41c70b6754b9a197dd2c22d4fa53d9b24b96c
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-11-27 11:43:23 +0530

    default parameters

--- tut1.js
+++ tut1.js
@@ -1,6 +1,22 @@
-function func(){
-    var a = 10;
-    console.log(a)
+// function func(){
+//     var a = 10;
+//     console.log(a)
+// }
+// func()
+// console.log(a)
+
+// let cannot be redeclared
+// let a = 20;
+// let a = 40;
+// console.log(a)
+
+function func1(a=10,b=10){
+    return a+b;
 }
-func()
-console.log(a)
+
+console.log(func1(20))
+
+
+
+
+
