43ac89ae25f15633731f6b0e08c8989f85d894f1
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-11-28 10:02:11 +0530

    encapsulation

--- classes.js
+++ classes.js
@@ -1,30 +1,58 @@
-class Person{
-    constructor(name,age){
-        this.name = name
-        this.age = age
-        console.log("constructor1")
-    }
+// class Person{
 
-    greet(){
-        console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old`)
-    }
-}
+//     constructor(name,age){
+//         this.name = name
+//         this.age = age
+//         console.log("constructor1")
+//     }
 
-class Employee extends Person{
-    constructor(name,age,position){
-        super(name,age)
-        this.position = position
-        console.log("constructor2")
-    }
+//     greet(){
+//         console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old`)
+//     }
+// }
 
-    greet2(){
-        console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old and I'm a ${this.position}`)
-    }
-}
+// class Employee extends Person{
+//     constructor(name,age,position){
+//         super(name,age)
+//         this.position = position
+//         console.log("constructor2")
+//     }
 
-const employee1 = new Employee("Rakshit",22,"Software Developer")
-employee1.greet2()
+//     greet2(){
+//         console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old and I'm a ${this.position}`)
+//     }
+// }
 
+// const employee1 = new Employee("Rakshit",22,"Software Developer")
+// employee1.greet2()
 
 // const persons = new Person("Rakshit",22)
 // persons.greet()
+
+//encapsulation
+
+class Car{
+    #speed
+    constructor(){
+        this.#speed = 0
+    }
+
+    accelerate(){
+        this.#speed += 10
+    }
+
+    getSpeed(){
+        return this.#speed
+    }
+
+    setSpeed(speed){
+        this.#speed = speed
+    }
+}
+
+const car = new Car()
+console.log(car.getSpeed())
+car.accelerate()
+car.setSpeed(100)
+console.log(car.getSpeed())
+// console.log(car.#speed)
